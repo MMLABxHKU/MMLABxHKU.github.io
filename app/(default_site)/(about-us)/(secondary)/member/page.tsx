b9b444feaea75ca67c20db631b49762ba8827aee
faikit <huijie.wang@hotmail.com>
2025-04-30 08:22:55 +0800

    .

--- app/(default_site)/(about-us)/(secondary)/member/page.tsx
+++ app/(default_site)/(about-us)/(secondary)/member/page.tsx
@@ -26,7 +26,7 @@ import { FadeIn } from "@/components/animation/fade-in"
 
 
 
-import { members } from "@/data/member"
+import { pis, members } from "@/data/member"
 const paths: { path: string; url: string; }[] = [
     {
         path: "MMLab",
@@ -88,8 +88,8 @@ export default function Home() {
                 {/* navigator */}
                 <div className="w-48 hidden 2xl:block">
                     <div className="w-full h-fit mt-20 flex flex-col gap-3 sticky top-64">
-                        {members.map((branch) => (
-                            <Link className="select-none flex items-center gap-3 group hover:text-amber-400" href={branch.branch_id_} key={branch.branch}>
+                        {pis.map((pi) => (
+                            <Link className="select-none flex items-center gap-3 group hover:text-mred" href={"#" + pi.id} key={pi.name}>
                                 <FadeIn>
                                     <span className="border-l-4 border-l-foreground text-sm">
                                         &nbsp;
@@ -97,7 +97,7 @@ export default function Home() {
                                 </FadeIn>
                                 <FadeIn>
                                     <span className="text-xl font-bold">
-                                        {branch.branch}
+                                        {pi.name}
                                     </span>
                                 </FadeIn>
                             </Link>
@@ -108,38 +108,81 @@ export default function Home() {
 
 
                 <div className="w-full max-w-7xl flex flex-col">
-                    {members.map((branch) => (
+                    {pis.map((pi) => (
                         <div>
-                            <FadeIn>
-                                <div className="w-full flex flex-col items-center">
-                                    <h2 className="w-full max-w-7xl mt-20 text-6xl font-bold"> 
-                                        <Link href={branch.branch_id_} className="scroll-mt-32 group flex items-center" id={branch.branch_id}>
-                                            {branch.branch}
-                                            <span className="ml-6 hidden group-hover:inline-block size-10 text-foreground">
-                                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
-                                                    <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
-                                                    <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
-                                                </svg>
-                                            </span>                        
-                                        </Link>
-                                    </h2>
-                                    <div className="w-full max-w-7xl mt-20 grid gap-10 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5">
-                                        {branch.person.map((member) => (
-                                            <div className="flex flex-col gap-3" key={member.name}>
-                                                <AspectRatio ratio={1/1}>
-                                                    <Image
-                                                        src={member.img}
-                                                        alt={member.name}
-                                                        fill
-                                                        className="h-full w-full rounded-md object-cover loading"
-                                                    />
-                                                </AspectRatio>
-                                                <span className="font-bold text-xl">{member.name}</span>
-                                                <span>{member.affiliation}</span>
-                                            </div>
-                                        ))}
+                            <FadeIn key={pi.name}>
+
+
+
+                                <div className="w-full max-w-7xl mt-20 flex flex-col gap-10 md:flex-row md:gap-20 justify-between">
+                                    <div className="flex-1/3 md:max-w-64">
+                                        <AspectRatio ratio={1/1}>
+                                            <Image
+                                                src={pi.img}
+                                                alt={pi.name}
+                                                fill
+                                                className="h-full w-full rounded-md object-cover loading"
+                                            />
+                                        </AspectRatio>
+                                    </div>
+                                    <div className="flex-2/3 flex flex-col gap-6">
+                                        <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold" id={pi.id}>
+                                            {pi.name}
+                                        </h1>
+                                        <p>
+                                            {pi.bio}
+                                        </p>
+                                        <div className="flex gap-6">
+                                            {pi.links.map((link) => (
+                                                <Link href={link.link} target="_blank" key={link.icon}  className="h-8 w-8 group">
+                                                    <AspectRatio ratio={1/1}>
+                                                        <Image
+                                                            src={"/icon/" + link.icon + ".svg"}
+                                                            alt={link.link}
+                                                            fill
+                                                            className="group-hover:scale-125 transition duration-200"
+                                                        />
+                                                    </AspectRatio>
+                                                </Link>
+                                            ))}
+                                        </div>
                                     </div>
                                 </div>
+
+
+                                <div className="w-full max-w-7xl mt-20 grid gap-10 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 2xl:grid-cols-5">
+                                    {[...members.values()].filter(member => member.pi.includes(pi.name)).map((member) => (
+                                        <div className="flex flex-col gap-3" key={member.name}>
+                                            <AspectRatio ratio={1/1}>
+                                                <Image
+                                                    src={member.img}
+                                                    alt={member.name}
+                                                    fill
+                                                    className="h-full w-full rounded-md object-cover loading"
+                                                />
+                                            </AspectRatio>
+                                            <span className="font-bold text-xl">{member.name}</span>
+                                            <span>{member.title}</span>
+                                            <div className="flex gap-6">
+                                                {member.links.map((link) => (
+                                                    <Link href={link.link} target="_blank" key={link.icon}  className="h-8 w-8 group">
+                                                        <AspectRatio ratio={1/1}>
+                                                            <Image
+                                                                src={"/icon/" + link.icon + ".svg"}
+                                                                alt={link.link}
+                                                                fill
+                                                                className="group-hover:scale-125 transition duration-200"
+                                                            />
+                                                        </AspectRatio>
+                                                    </Link>
+                                                ))}
+                                            </div>
+                                        </div>
+                                    ))}
+                                </div>
+
+
+
                             </FadeIn>
                             <div className="w-full flex flex-col items-center">
                                 <Separator className="max-w-7xl mt-20"/>
--- data/member.tsx
+++ data/member.tsx
@@ -1,5 +1,6 @@
 export const pis: { 
     name: string; 
+    id: string;
     img: string; 
     title: string; 
     bio: string;  
@@ -7,6 +8,7 @@ export const pis: {
 }[] = [
     {
         name: "Ping Luo",
+        id: "ping_luo",
         img: "https://mmlabassets.github.io/member/ping_luo_c.jpg",
         title: "Associate Professor",
         bio: "Ping Luo is an Associate Professor in the Department of Computer Science at the University of Hong Kong, an Associate Director of the HKU Musketeers Foundation Institute of Data Science (HKU IDS), and a Deputy Director of the Joint Research Lab of HKU and Shanghai AI Lab. He obtained his Ph.D. in Information Engineering from the Chinese University of Hong Kong in 2014, under the supervision of Prof. Xiaoou Tang (founder of SenseTime) and Prof. Xiaogang Wang. Before joining HKU in 2019, he was a Research Director in SenseTime. He has published 100+ papers in international conferences and journals such as TPAMI, ICML, ICLR, NeurIPS, and CVPR, with over 50,000 citations on Google Scholar. He was awarded the 2015 AAAI Easily Accessible Paper, nominated for the 2022 Computational Visual Media Journal's Best Paper of the Year, won the 2022 ACL Outstanding Paper, the 2023 World Artificial Intelligence Conference (WAIC) Outstanding Papers, and was a candidate for the Best Paper at ICCV’23. He was recognized as one of the innovators under 35 in the Asia-Pacific region by the MIT Technology Review (MIT TR35) in 2020. He has mentored 30 Ph.D. students, many of whom have received significant awards such as the Nvidia Fellowship, Baidu Fellowship, WAIC Yunfan Award, etc.",
@@ -27,6 +29,7 @@ export const pis: {
     },
     {
         name: "Hongyang Li",
+        id: "hongyang_li",
         img: "https://mmlabassets.github.io/member/hongyang_li_c.jpg",
         title: "Associate Professor",
         bio: "Hongyang Li is an Assistant Professor at Musketeers Foundation Institute of Data Science, University of Hong Kong and has led OpenDriveLab since 2021. His research focus is on autonomous driving and embodied AI. He led the end-to-end autonomous driving project, UniAD and won the IEEE CVPR 2023 Best Paper Award. UniAD has a tremendous impact both in academia and industry, including the recent rollout to customers by Tesla in FSD V12. He created the first large-scale real robot ecosystem, AgiBot World, that systematically investigated the scaling law principles for robotic manipulation. He proposed the bird's-eye-view perception work, BEVFormer, that won Top 100 AI Papers in 2022. He served as Area Chair for CVPR, NeurIPS (including 2023 Notable AC), ICLR, ICCV, ICML, RSS, referee for Nature Communications, Guest Editor at Automotive Innovations. He is the Working Group Chair for IEEE Standards P3474 under Vehicular Technology Society. He is the Senior Member of IEEE, CCF and CSIG. He is the recipient of China AI Wu Wen Jun Early Career Award 2024.",
@@ -51,6 +54,7 @@ export const pis: {
     },
     {
         name: "Xihui Liu",
+        id: "xihui_liu",
         img: "https://mmlabassets.github.io/member/ping_luo_c.jpg",
         title: "xxx",
         bio: "Hongyang Li is an Assistant Professor at Musketeers Foundation Institute of Data Science, University of Hong Kong and has led OpenDriveLab since 2021. His research focus is on autonomous driving and embodied AI. He led the end-to-end autonomous driving project, UniAD and won the IEEE CVPR 2023 Best Paper Award. UniAD has a tremendous impact both in academia and industry, including the recent rollout to customers by Tesla in FSD V12. He created the first large-scale real robot ecosystem, AgiBot World, that systematically investigated the scaling law principles for robotic manipulation. He proposed the bird's-eye-view perception work, BEVFormer, that won Top 100 AI Papers in 2022. He served as Area Chair for CVPR, NeurIPS (including 2023 Notable AC), ICLR, ICCV, ICML, RSS, referee for Nature Communications, Guest Editor at Automotive Innovations. He is the Working Group Chair for IEEE Standards P3474 under Vehicular Technology Society. He is the Senior Member of IEEE, CCF and CSIG. He is the recipient of China AI Wu Wen Jun Early Career Award 2024.",
@@ -63,6 +67,7 @@ export const pis: {
     },
     {
         name: "Bo Dai",
+        id: "bo_dai",
         img: "https://mmlabassets.github.io/member/ping_luo_c.jpg",
         title: "xxx",
         bio: "Hongyang Li is an Assistant Professor at Musketeers Foundation Institute of Data Science, University of Hong Kong and has led OpenDriveLab since 2021. His research focus is on autonomous driving and embodied AI. He led the end-to-end autonomous driving project, UniAD and won the IEEE CVPR 2023 Best Paper Award. UniAD has a tremendous impact both in academia and industry, including the recent rollout to customers by Tesla in FSD V12. He created the first large-scale real robot ecosystem, AgiBot World, that systematically investigated the scaling law principles for robotic manipulation. He proposed the bird's-eye-view perception work, BEVFormer, that won Top 100 AI Papers in 2022. He served as Area Chair for CVPR, NeurIPS (including 2023 Notable AC), ICLR, ICCV, ICML, RSS, referee for Nature Communications, Guest Editor at Automotive Innovations. He is the Working Group Chair for IEEE Standards P3474 under Vehicular Technology Society. He is the Senior Member of IEEE, CCF and CSIG. He is the recipient of China AI Wu Wen Jun Early Career Award 2024.",
@@ -77,71 +82,45 @@ export const pis: {
 
 
 export const members: { 
-    branch: string; 
-    branch_id: string;
-    branch_id_: string;
-    person: { 
-        name: string; 
-        img: string; 
-        affiliation: string 
-    }[]; 
+    name: string; 
+    img: string; 
+    title: string; 
+    pi: string[];
+    links: { icon: string; link: string; }[];
 }[]  = [
     {
-        branch: "@ HKU",
-        branch_id : "at_hku",
-        branch_id_ : "#at_hku",
-        person: [
+        name: "Chonghao Sima",
+        img: "https://mmlabassets.github.io/member/chonghao_sima_c.jpg",
+        title: "Ph.D. Candidate",
+        pi: ["Hongyang Li", "Ping Luo"],
+        links: [
             {
-                name: "Ping Luo",
-                img: "/person/ping_luo.jpg",
-                affiliation: "HKU",
-            },
-            {
-                name: "Hongyang Li",
-                img: "/person/hongyang_li.jpg",
-                affiliation: "HKU",
-            },
-            {
-                name: "Hongyang Li",
-                img: "/person/hongyang_li.jpg",
-                affiliation: "HKU",
-            },
-            {
-                name: "Hongyang Li",
-                img: "/person/hongyang_li.jpg",
-                affiliation: "HKU",
-            },
-            {
-                name: "Ping Luo",
-                img: "/person/ping_luo.jpg",
-                affiliation: "HKU",
-            },
-            {
-                name: "Ping Luo",
-                img: "/person/ping_luo.jpg",
-                affiliation: "HKU",
+                icon: "webpage",
+                link: "xxx",
             },
         ],
     },
     {
-        branch: "@ CUHK",
-        branch_id : "at_cuhk",
-        branch_id_ : "#at_cuhk",
-        person: [
+        name: "Yixuan Pan",
+        img: "https://mmlabassets.github.io/member/yixuan_pan_c.jpg",
+        title: "Ph.D. Candidate",
+        pi: ["Hongyang Li"],
+        links: [
             {
-                name: "Ping Luo",
-                img: "/person/ping_luo.jpg",
-                affiliation: "HKU",
+                icon: "webpage",
+                link: "xxx",
             },
+        ],
+    },
+    {
+        name: "Li Chen",
+        img: "https://mmlabassets.github.io/member/li_chen_c.jpg",
+        title: "Ph.D. Candidate",
+        pi: ["Hongyang Li"],
+        links: [
             {
-                name: "Ping Luo",
-                img: "/person/ping_luo.jpg",
-                affiliation: "HKU",
-            },
-            {
-                name: "Ping Luo",
-                img: "/person/ping_luo.jpg",
-                affiliation: "HKU",
+                icon: "webpage",
+                link: "xxx",
             },
         ],
     },
