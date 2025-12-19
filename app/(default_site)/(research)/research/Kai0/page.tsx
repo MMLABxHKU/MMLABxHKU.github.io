24fa3651e606601163c3787f51377f94c87732cf
faikit <huijie.wang@hotmail.com>
2025-12-19 09:07:34 +0800

    .

--- app/(default_site)/(research)/research/Kai0/page.tsx
+++ app/(default_site)/(research)/research/Kai0/page.tsx
@@ -128,6 +128,14 @@ export default function Home() {
                                     Stage Advantage
                                 </span>
                             </Link>
+                            <Link className="select-none flex items-center gap-3 group" href="#side_note">
+                                <span className="border-l-2 border-l-mgray text-sm group-hover:border-l-mred">
+                                    &nbsp;
+                                </span>
+                                <span className="text-sm text-mgray group-hover:text-mred">
+                                    Side Note
+                                </span>
+                            </Link>
                             <Link className="select-none flex items-center gap-3 group" href="#data">
                                 <span className="border-l-2 border-l-mgray text-sm group-hover:border-l-mred">
                                     &nbsp;
@@ -812,6 +820,26 @@ export default function Home() {
                                 Inference Optimization Methods Comparison / Figure description TODO
                             </i>
                             <div></div>
+                        </div>
+
+
+
+                        <div className="w-full flex flex-col items-center px-6">
+                            <h2 className="w-full max-w-4xl mt-20 text-3xl font-bold"> 
+                                <Link href="#side_note" className="scroll-mt-32 group flex items-center flex-row" id="side_note">
+                                    <h2>
+                                        Side Note
+                                    </h2>
+                                    <div className="min-w-12 select-none">
+                                        <span className="ml-6 hidden group-hover:inline-block size-6 text-foreground">
+                                            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                                                <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
+                                                <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
+                                            </svg>
+                                        </span> 
+                                    </div>
+                                </Link>
+                            </h2>
                         </div>
 
                         
