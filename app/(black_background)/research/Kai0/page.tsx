b28f329bb227d0e94ea4be4b9e0e2da1e83d0112
Chonghao Sima <simac@purdue.edu>
2025-12-21 16:27:53 +0800

    update demo video link

--- app/(black_background)/research/Kai0/page.tsx
+++ app/(black_background)/research/Kai0/page.tsx
@@ -211,7 +211,7 @@ export default function Home() {
                                 poster="https://assets.kinetixai.cn/FoldAnything/full.png"
                                 className="w-full aspect-video rounded-sm"
                             >
-                                <source src="https://assets.kinetixai.cn/FoldAnything/full_Dec21_1427.mp4" type="video/mp4" />
+                                <source src="https://assets.kinetixai.cn/FoldAnything/full_Dec21_compress_round.mp4" type="video/mp4" />
                             </video>
                         </div>
                         {/* <div className="mt-3 flex flex-row gap-6 justify-center">
