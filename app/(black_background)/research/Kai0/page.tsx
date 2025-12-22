a838111cd3bf43cbbe600b63273d4a683836d166
faikit <huijie.wang@hotmail.com>
2025-12-22 16:35:14 +0800

    .

--- app/(black_background)/research/Kai0/page.tsx
+++ app/(black_background)/research/Kai0/page.tsx
@@ -361,7 +361,7 @@ export default function Home() {
                         <div className="px-6">
                             <video
                                 autoPlay muted loop playsInline
-                                poster="https://assets.kinetixai.cn/FoldAnything/full.png"
+                                poster="https://assets.kinetixai.cn/FoldAnything/pipeling.jpg"
                                 className="w-full aspect-video rounded-sm"
                             >
                                 <source src="https://assets.kinetixai.cn/FoldAnything/pipeling.mp4" type="video/mp4" />
