9adfd23241b8b4b1c7563bb3472f9623475b35ee
faikit <huijie.wang@hotmail.com>
2025-12-23 13:28:11 +0800

    .

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,16 @@
 import type { Metadata } from "next";
 export const metadata: Metadata = {
-    title: "404 | MMLab",
-    description: "404 Not Found",
-    keywords: ["404", "MMLab", "Multimedia Laboratory", "HKU", "CUHK", "NTU"],
+    title: "redirecting",
 };
 
 
 
-import NotFound404 from '@/components/ui/404'
+import Redirect from '@/components/redirect'
 
 
 
 export default function NotFound() {
     return (
-        <NotFound404/>
+        <Redirect/>
     )
 }
