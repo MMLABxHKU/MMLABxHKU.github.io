5baf850c4feef75c21e73d2e89284bd251a002a6
faikit <huijie.wang@hotmail.com>
2026-01-30 16:18:30 +0800

    .

---
+++ app/(black_background)/research/MMHand/page.tsx
@@ -0,0 +1,81 @@
+import type { Metadata } from "next";
+export const metadata: Metadata = {
+    title: "MM Hand | HKU MMLab",
+    description: "Multimedia Laboratory",
+    keywords: ["MM Hand", "MMLab", "Multimedia Laboratory", "HKU", "CUHK", "NTU"],
+};
+
+
+
+
+import Image from 'next/image'
+import Link from "next/link"
+import { Slash } from "lucide-react"
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+import { Separator } from "@/components/ui/separator"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+
+
+const paths: { path: string; url: string; }[] = [
+    {
+        path: "MMLab",
+        url: "/"
+    },
+]
+
+
+
+export default function Home() {
+    return (
+        <main>
+            <div className="w-full">
+
+
+
+                <div className="w-full h-svh overflow-hidden">
+                    <video autoPlay muted loop playsInline poster="https://assets.kinetixai.cn/kinetixai/index/banner-20251212.jpg" className="w-full h-full object-cover">
+                        <source src="https://assets.kinetixai.cn/AMS/251212_raw.mp4" type="video/mp4" />
+                    </video>
+                </div>
+
+
+
+                {/* <div className="w-full px-6 flex justify-center mt-36">
+                    <div className="w-full max-w-3xl flex">
+                        <h2 className="text-t1"> 
+                            <Link href="#features" className="scroll-mt-32 group flex items-center" id="features">
+                                Features
+                                <span className="ml-6 hidden group-hover:inline-block size-6 text-foreground">
+                                    <svg viewBox="0 0 24 24" fill="none" stroke="currentColor">
+                                        <path d="M15.197 3.35462C16.8703 1.67483 19.4476 1.53865 20.9536 3.05046C22.4596 4.56228 22.3239 7.14956 20.6506 8.82935L18.2268 11.2626M10.0464 14C8.54044 12.4882 8.67609 9.90087 10.3494 8.22108L12.5 6.06212" strokeWidth="2.5" strokeLinecap="round"/>
+                                        <path d="M13.9536 10C15.4596 11.5118 15.3239 14.0991 13.6506 15.7789L11.2268 18.2121L8.80299 20.6454C7.12969 22.3252 4.55237 22.4613 3.0464 20.9495C1.54043 19.4377 1.67609 16.8504 3.34939 15.1706L5.77323 12.7373" strokeWidth="2.5" strokeLinecap="round"/>
+                                    </svg>
+                                </span>
+                            </Link>
+                        </h2>
+                    </div>
+                </div> */}
+
+
+
+                <div className="w-full px-6 flex justify-center mt-36">
+                    <div className="w-full max-w-3xl flex flex-col gap-6 leading-relaxed">
+                        <p className="text-t1">
+                            To be released.
+                        </p>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </main>
+    );
+}
